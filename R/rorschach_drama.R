887bf9c7381561f49cfd44e6b3e6b7b07ad83a62
Stefan <stefan.b.fleck@gmail.com>
2020-11-02 21:21:03 +0000

    generate mirror pipeline programmatically

--- R/rorschach_drama.R
+++ R/rorschach_drama.R
@@ -22,8 +22,10 @@ rorschach_drama <- function(
   tune = "film", # "film", "animation",
   batch_size = 50,
   temp_dir = tempdir(),
-  rotate = glue("2*PI*t/10:ow={resolution[[1]]}:oh={resolution[[2]]}:c=none"),
-  hue = "H=2*PI*t: s=sin(2*PI*t)+1",
+  filters = list(
+    glue("rotate=2*PI*t/10:ow={resolution[[1]]}:oh={resolution[[2]]}:c=none"),
+    "hue=H=2*PI*t: s=sin(2*PI*t)+1"
+  ),
   start_batch = 1L  # NULL for no clip creation
 ){
   stopifnot(
@@ -31,7 +33,7 @@ rorschach_drama <- function(
     is_scalar_bool(overwrite),
     dir.exists(dirname(outfile)),
     is.numeric(resolution) && identical(length(resolution) , 2L),
-    is_scalar_character(mirror) && mirror %in% names(MIRROR_PRESETS),
+    is_scalar_character(mirror),
     is_scalar_character(tune),
     is_scalar_integerish(batch_size),
     dir.exists(temp_dir),
@@ -69,7 +71,9 @@ rorschach_drama <- function(
     )
 
     # mirror args
-    mirror <- MIRROR_PRESETS[[mirror]]
+    if (mirror %in% names(MIRROR_PRESETS))
+      mirror <- MIRROR_PRESETS[[mirror]]
+
     lg$info("processing {length(batches) + start_batch - 1L} batches")
     pb$tick(start_batch, tokens = list(chunk_eta = "NA"))
     cat("\n")
@@ -79,15 +83,15 @@ rorschach_drama <- function(
     for (i in seq_along(batches)){
       inf  <- paste("-i", batches[[i]], collapse = " ")
       outf <- tempfiles[[i]]
+      batch_number <- i + start_batch - 1L  # for logging
+      ids <- seq_along(batches[[i]]) - 1L
 
       lg$info(
-        "processing batch {cur}/{all}",
-        cur = i + start_batch - 1L,
+        "processing batch {cur}/{all} ({length(ids)} files)",
+        cur = batch_number,
         all = length(batches) + start_batch - 1L,
         file = outf
       )
-
-      ids <- seq_along(batches[[i]]) - 1L
 
       scale <-  paste(
         glue("[{ids}:v:0]scale='if(gt(a*sar,{resolution[[1]]}/{resolution[[2]]}),{resolution[[1]]},{resolution[[2]]}*iw*sar/ih)':'if(gt(a*sar,16/9),{resolution[[1]]}*ih/iw/sar,{resolution[[2]]})', pad={resolution[[1]]}:{resolution[[2]]}:(ow-iw)/2:(oh-ih)/2,setsar=1[v{ids}]"),
@@ -101,18 +105,27 @@ rorschach_drama <- function(
       assert(all(file.exists(batches[[i]])))
       outf_tmp <- paste0(tools::file_path_sans_ext(outf), "_tmp.mkv")
 
+      stopifnot(
+        is_scalar_character(inf),
+        is_scalar_character(mirror),
+        is_scalar_character(video_codec),
+        is_scalar_character(outf_tmp)
+      )
+
+
       args <- glue(
         '{inf} -y -filter_complex "
         color=size={resolution[[1]]*2}x{resolution[[2]]*2}:color=black [bgr];\
         {scale};\
         {concat}[out];\
         [out]{mirror}[out];\
-        [out]rotate={rotate}[out];\
-        {if (!is.null(hue)) "[out]hue={hue}[out]" else ""}
+        {paste("[out]", filters, "[out]", collapse = ";", sep = "")}
         " -map [out] {outf_tmp} -c:v {video_codec}'
       )
 
       t1 <- Sys.time()
+
+      lg$debug("calling ffmpeg", args = gsub("\n", "", args))
 
       ret <- try({system2(
         "ffmpeg",
@@ -125,11 +138,17 @@ rorschach_drama <- function(
 
       if (is_try_error(ret) || !identical(as.integer(ret), 0L)){
         walk(tail(readLines(stderr_log)), lg$fatal)
-        lg$fatal("skipping batch {i} because of ffmpeg error; please check log files.", temp_file = outf_tmp)
-        next
+        lg$fatal(
+          "skipping batch {batch_number} because of ffmpeg error; please check log files.",
+          temp_file = outf_tmp,
+          args = args
+        ) %>%
+          stop()
       }
 
       file.rename(outf_tmp, outf)
+
+      lg$debug("finished processing batch {batch_number}")
       pb$tick()
     }
   }
@@ -139,6 +158,16 @@ rorschach_drama <- function(
 
 
 
+#' Title
+#'
+#' @param infiles
+#' @param outfile
+#' @param temp_dir
+#'
+#' @return
+#' @export
+#'
+#' @examples
 concatennate_media <- function(
   infiles,
   outfile,
@@ -220,8 +249,109 @@ MIRROR_PRESETS <- list(
     [mid][inf] overlay=0:3*h     [mid];\
     [mid][in4] overlay=w:3*h    [mid];\
     [mid][ind] overlay=2*w:3*h   [mid];\
-    [mid][ine] overlay=3*w:3*h"
+    [mid][ine] overlay=3*w:3*h",
+  "24" =
+    "split=24 [in0][in1][in2][in3][in4][in5][in6][in7][in8][in9][ina][inb][inc][ind][ine][inf][ing][inh][ini][inj][ink][inl]; \
+    [in0] crop=iw/2:ih/2:0:0 [in0];\
+    [in1] crop=iw/2:ih/2:0:0, hflip [in1]; \
+    [in2] crop=iw/2:ih/2:0:0, vflip [in2]; \
+    [in3] crop=iw/2:ih/2:0:0, vflip, hflip [in3]; \
+    [in4] crop=iw/2:ih/2:0:0 [in4];\
+    [in5] crop=iw/2:ih/2:0:0, hflip [in5]; \
+    [in6] crop=iw/2:ih/2:0:0, vflip [in6]; \
+    [in7] crop=iw/2:ih/2:0:0, vflip, hflip [in7]; \
+    [in8] crop=iw/2:ih/2:0:0 [in8];\
+    [in9] crop=iw/2:ih/2:0:0, hflip [in9]; \
+    [ina] crop=iw/2:ih/2:0:0, vflip [ina]; \
+    [inb] crop=iw/2:ih/2:0:0, vflip, hflip [inb]; \
+    [inc] crop=iw/2:ih/2:0:0 [inc];\
+    [ind] crop=iw/2:ih/2:0:0, hflip [ind]; \
+    [ine] crop=iw/2:ih/2:0:0, vflip [ine]; \
+    [inf] crop=iw/2:ih/2:0:0, vflip, hflip [inf]; \
+    [ing] crop=iw/2:ih/2:0:0 [ing];\
+    [inh] crop=iw/2:ih/2:0:0, hflip [inh]; \
+    [ini] crop=iw/2:ih/2:0:0, vflip [ini]; \
+    [inj] crop=iw/2:ih/2:0:0, vflip, hflip [inj]; \
+    [ink] crop=iw/2:ih/2:0:0 [ink];\
+    [inl] crop=iw/2:ih/2:0:0, hflip [inl]; \
+    [bgr][in1] overlay=0:2:shortest=1 [mid];\
+    [mid][in2] overlay=w:0    [mid];\
+    [mid][in3] overlay=2*w:0   [mid];\
+    [mid][in0] overlay=3*w:0   [mid];\
+    [mid][in7] overlay=0:h     [mid];\
+    [mid][in8] overlay=w:h    [mid];\
+    [mid][in5] overlay=2*w:h   [mid];\
+    [mid][in6] overlay=3*w:h   [mid];\
+    [mid][in9] overlay=0:2*h     [mid];\
+    [mid][ina] overlay=w:2*h    [mid];\
+    [mid][inb] overlay=2*w:2*h   [mid];\
+    [mid][inc] overlay=3*w:2*h   [mid];\
+    [mid][inf] overlay=0:3*h     [mid];\
+    [mid][in4] overlay=w:3*h    [mid];\
+    [mid][ind] overlay=2*w:3*h   [mid];\
+    [mid][ine] overlay=3*w:3*h [mid];\
+    [mid][ing] overlay=4*w:1*h     [mid];\
+    [mid][inh] overlay=4*w:2*h    [mid];\
+    [mid][ini] overlay=4*w:3*h   [mid];\
+    [mid][inj] overlay=5*w:1*h [mid];\
+    [mid][ink] overlay=5*w:2*h     [mid];\
+    [mid][inl] overlay=5*w:3*h"
 )
+
+
+make_mirror <- function(n){
+  idx <- seq_len(n)
+
+  streams <- glue("[in{idx}a][in{idx}b][in{idx}c][in{idx}d]") %>%
+    paste(collapse = "")
+
+  crop <- glue("
+    [in{idx}b] crop=iw/2:ih/2:0:0 [in{idx}b];\
+    [in{idx}a] crop=iw/2:ih/2:0:0, hflip [in{idx}a]; \
+    [in{idx}c] crop=iw/2:ih/2:0:0, vflip [in{idx}c]; \
+    [in{idx}d] crop=iw/2:ih/2:0:0, vflip, hflip [in{idx}d]; \
+  ") %>%
+    paste(collapse = "\n")
+
+  dd <- list()
+  w <- 0
+  h <- 0
+
+  for (i in idx){
+
+    if (i %% 4L == 0L){
+      w <-  w + 2L
+      order <- c("a", "b", "c", "d")
+    } else if (i %% 3L == 0L){
+      h <- h + 2L
+      w <- w - 2L
+      order <- c("b", "a", "d", "c")
+    } else if (i %% 2L == 0L){
+      w <- w + 2L
+      order <- c("c", "d", "a", "b")
+    } else {
+      order <- c("a", "b", "c", "d")
+    }
+    w
+    h
+    dd[[i]] <- glue("
+      [bgr][in{i}{order[[1]]}] overlay=w*{w}:h*{h}:shortest=1 [bgr];\
+      [bgr][in{i}{order[[2]]}] overlay=w*{w+1}:h*{h}    [bgr];\
+      [bgr][in{i}{order[[3]]}] overlay=w*{w}:h*{h+1}   [bgr];\
+      [bgr][in{i}{order[[4]]}] overlay=w*{w+1}:h*{h+1} [bgr];\
+    ")
+  }
+
+  overlay <- paste(unlist(dd), collapse = "\n")
+
+  res <- glue(
+    "split={n*4} {streams};\n",
+    crop, "\n",
+    overlay
+  )
+
+  sub("\\[bgr\\];$", "", res)
+}
 
 
 
